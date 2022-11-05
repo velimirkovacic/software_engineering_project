fd9c19a470354e91443ca16bcfe9c0b7318407bd
velimir-kovacic <vk53391@fer.hr>
2022-11-05 12:43:47 +0100

    dodana backedn struktura

---
+++ IzvorniKod/backend/src/main/java/vidoje/eventko/EventkoApplication.java
@@ -0,0 +1,13 @@
+package vidoje.eventko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventkoApplication.class, args);
+	}
+
+}
