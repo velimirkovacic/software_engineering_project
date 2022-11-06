e5156b48c1cb7b3b545be4811322d70afe01a663
velimir-kovacic <vk53391@fer.hr>
2022-11-07 00:12:55 +0100

    Modelirani entiteti

---
+++ IzvorniKod/backend/src/main/java/vidoje/eventko/domain/EventType.java
@@ -0,0 +1,53 @@
+package vidoje.eventko.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Vrsta")
+public class EventType {
+    @Id
+    @Column(name = "id_vrsta", insertable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "naziv_vrsta", insertable = false, updatable = false)
+    private String name;
+
+    @Column(name = "opis_vrsta", insertable = false, updatable = false)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "EventType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
