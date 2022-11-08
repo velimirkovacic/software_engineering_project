59683c30518fe9305b2a70fe61329dc8854ed5e2
velimir-kovacic <vk53391@fer.hr>
2022-11-08 23:39:01 +0100

    Izbrisane greskom dodane stavke u main

--- IzvorniKod/backend/src/main/java/vidoje/eventko/repos/EventRepo.java
+++
@@ -1,8 +0,0 @@
-package vidoje.eventko.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import vidoje.eventko.domain.Event;
-
-public interface EventRepo extends JpaRepository<Event, Long> {
-
-}
--- IzvorniKod/backend/src/main/java/vidoje/eventko/service/EventService.java
+++
@@ -1,9 +0,0 @@
-package vidoje.eventko.service;
-
-import vidoje.eventko.domain.Event;
-
-import java.util.List;
-
-public interface EventService {
-    List<Event> listAll();
-}
