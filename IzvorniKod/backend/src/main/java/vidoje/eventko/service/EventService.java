cab719a5fc58932077d10752452d6b6237c9cc8d
velimir-kovacic <vk53391@fer.hr>
2022-11-10 17:03:48 +0100

    Dodani neki javadocovi

--- IzvorniKod/backend/src/main/java/vidoje/eventko/service/EventService.java
+++ IzvorniKod/backend/src/main/java/vidoje/eventko/service/EventService.java
@@ -5,5 +5,10 @@ import vidoje.eventko.domain.Event;
 import java.util.List;
 
 public interface EventService {
+
+    /**
+     * Dohvaćanje svih događaja u sustavu
+     * @return lista objekata tipa Event
+     */
     List<Event> listAll();
 }
