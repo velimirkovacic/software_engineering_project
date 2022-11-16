6e2483cdad0b2fcc85dfa99d5b48a27945f3ab40
velimir-kovacic <vk53391@fer.hr>
2022-11-16 19:43:21 +0100

    Dodane stavke za deployment error

--- IzvorniKod/backend/src/main/java/vidoje/eventko/EventkoApplication.java
+++ IzvorniKod/backend/src/main/java/vidoje/eventko/EventkoApplication.java
@@ -2,11 +2,12 @@ package vidoje.eventko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 import java.io.Console;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class EventkoApplication {
 
 	public static void main(String[] args) {
