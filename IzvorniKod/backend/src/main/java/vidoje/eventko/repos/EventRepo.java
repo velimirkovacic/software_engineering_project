f70a3beabce2ca92faa4ad17d2edc57302a353a6
velimir-kovacic <vk53391@fer.hr>
2022-11-16 22:33:23 +0100

    Dodane anotacije za repozitorije

--- IzvorniKod/backend/src/main/java/vidoje/eventko/repos/EventRepo.java
+++ IzvorniKod/backend/src/main/java/vidoje/eventko/repos/EventRepo.java
@@ -1,8 +1,10 @@
 package vidoje.eventko.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vidoje.eventko.domain.Event;
 
+@Repository
 public interface EventRepo extends JpaRepository<Event, Long> {
 
 }
--- IzvorniKod/backend/src/main/java/vidoje/eventko/repos/UserRepo.java
+++ IzvorniKod/backend/src/main/java/vidoje/eventko/repos/UserRepo.java
@@ -1,10 +1,12 @@
 package vidoje.eventko.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vidoje.eventko.domain.User;
 
 import java.util.List;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     List<User> findByUsername(String username);
 
