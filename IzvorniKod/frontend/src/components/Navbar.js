26a3a845cd231a71bf604818ec13e82afc3b23d2
ema nekic <ema.nekic@fer.hr>
2022-12-17 14:41:34 +0100

    dodana kartica u navbar ako je mod

--- IzvorniKod/frontend/src/components/Navbar.js
+++ IzvorniKod/frontend/src/components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import myImage from '../Slike/eventkoLogo.png'
 import { ReactSession } from 'react-client-session';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 function Navbar() {
     const username = ReactSession.get("username");
+
+
+
+
+    const roles = ReactSession.get("roles");
+    let isMod = false;
+    console.log(roles)
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'Moderator') {
+            isMod = true;
+            console.log("DSDFFFVDBLČ")
+            console.log(isMod)
+        }
+    }
+
 
     function notifications() {
         const options = {
@@ -65,6 +81,7 @@ function Navbar() {
                     <li><a href="/notifications" onClick={notifications}>Obavijesti</a></li>
                     <li><a>Moji Prijatelji</a></li>
                     <li><a href="/attended" onClick={attended}>Pohađani Eventi</a></li>
+                    {isMod && <li><a href="/userActions">Upravljaj korisnicima</a> </li>}
                     <div className='userInfo'>
                         <li><a href="/profile" onClick={profileDetails}>{username}</a></li>
                         <li><a href="/" onClick={odjavi}>Odjava</a></li>
