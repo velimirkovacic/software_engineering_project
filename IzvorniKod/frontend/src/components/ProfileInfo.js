48dc443d201add03a5100f6ae46ee44adfcaacf5
Ana <ana.maric@fer.hr>
2022-12-17 14:20:10 +0100

    ProfileInfo, SignUp povratak itd

--- IzvorniKod/frontend/src/components/ProfileInfo.js
+++ IzvorniKod/frontend/src/components/ProfileInfo.js
@@ -2,7 +2,36 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { ReactSession } from 'react-client-session';
 
-function ProfileInfo() {
+const ProfileInfo = () => {
+
+    console.log(ReactSession.get("nickname"));
+    const [details, setDetails] = useState({nickname:ReactSession.get("nickname")});
+
+    const handleProfile = e => {
+        e.preventDefault();
+        //ovdje u bazi spremanje novog nicknamea 
+        const data = {
+            nickname : details.nickname, 
+        };
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/JSON'
+            },
+            body: JSON.stringify(data)
+        };
+        console.log(data)
+        fetch('/api/user/nickname', options) 
+            .then(response => {
+                console.log(response)
+                if (response.ok) {
+                    response.json().then(json => {
+                        console.log(json)
+                        alert('Uspješna promjena nadimka')
+                    })
+                }
+            })
+    };
 
     function povratak() {
         const options = {
@@ -18,10 +47,25 @@ function ProfileInfo() {
     }
 
     return (
-            <div>
-            <a>OVO JE VAŠ PROFIL</a>
-            <a href="/" onClick={povratak}>Povratak</a>
+        <form onSubmit={handleProfile}>
+            <div className="App">
+                    <div className='form-inner'>
+                        <h2>Vaš korisnički profil</h2>
+                        <div className='form-group'>
+                            <label name='profile'>Nadimak: </label>
+                            <input type='text' name='nickname' id='nickname' onChange={e => setDetails({ ...details, nickname: e.target.value })} value={details.nickname}/> 
+                        </div>
+                        <div className='form-group'>
+                            <label name='profile'>Korisničko ime: {ReactSession.get("username")}</label>
+                        </div>
+                        <div className='form-group'>
+                            <label name='profile'>E-mail: {ReactSession.get("email")}</label>
+                        </div>
+                        <button type='submit' name='register'>Spremi promjene</button>
+                        <a href='/' onClick={povratak}>Vrati se natrag </a>
+                    </div>
             </div>
+        </form>
       );
 }
 
