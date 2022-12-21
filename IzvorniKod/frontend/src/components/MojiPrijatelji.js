1bba709c8394c1bf644927a0fcf62de970051e32
ema nekic <ema.nekic@fer.hr>
2022-12-21 02:00:05 +0100

    pobrisane nepotrebne stvari iz Obavijesti, Pohadani eventi, Moji Prijatelji

--- IzvorniKod/frontend/src/components/MojiPrijatelji.js
+++ IzvorniKod/frontend/src/components/MojiPrijatelji.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { ReactSession } from 'react-client-session';
 import myImage from '../Slike/eventkoLogo.png'
 import { useEffect } from 'react';
+import Navbar from './Navbar';
 function MojiPrijatelji() {
     const [userData, setUserData] = useState({ username: '', moderator: false, admin: false })
 
@@ -33,64 +34,16 @@ function MojiPrijatelji() {
         getUserData()
     }, [])
 
-    function odjavi() {
-        ReactSession.set("isLoggedIn", "false");
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/JSON'
-            }
-        };
-        fetch('/api/user/logout', options)
-            .then(response => {
-                console.log(response)
-            });
-    }
-
-    /*
-    const getPromotedEvents = () => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/JSON'
-            }
-        };
-        fetch('/api/events/promoted', options)
-            .then(response => {
-              response.json().then(json => {
-                console.log(json)
-                const helpArray = []
-                json.userAvailableEvents.map(ev => helpArray.push(ev))
-                setPromoted(helpArray)
-            })
-        })
-    }
-    */
 
     return (
         <body>
-            <nav className='nav'>
-                <a href="/"><img src={myImage} alt="" /></a>
-                <div className='navComp'>
-                    <ul>
-                        <li><a href="/notifications">Obavijesti</a></li>
-                        <li><a href="/moji_prijatelji">Moji Prijatelji</a></li>
-                        <li><a href="/attended">Pohađani Eventi</a></li>
-                        {(userData.moderator === true) ? (<li><a href="/userActions">Upravljaj korisnicima</a></li>) : ('')}
-                        {(userData.admin === true) ? (<li><a href="/admin">Administrativne ovlasti</a></li>) : ('')}
-                        <div className='userInfo'>
-                            {(userData.username !== '') ? (<li><a href="/profile">{userData.username}</a></li>) : ('')}
-                            <li><a href="/" onClick={odjavi}>Odjava</a></li>
-                        </div>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar />
             <div id="container">
                 <a>Nazalost nemate prijatelja</a>
                 <a href="/">Povratak</a>
             </div>
         </body>
-      );
+    );
 }
 
 export default MojiPrijatelji;
--- IzvorniKod/frontend/src/components/Notifications.js
+++ IzvorniKod/frontend/src/components/Notifications.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { ReactSession } from 'react-client-session';
 import myImage from '../Slike/eventkoLogo.png'
 import { useEffect } from 'react';
+import Navbar from './Navbar';
 function Notifications() {
     const [userData, setUserData] = useState({ username: '', moderator: false, admin: false })
 
@@ -34,43 +35,15 @@ function Notifications() {
         getUserData()
     }, [])
 
-    function odjavi() {
-        ReactSession.set("isLoggedIn", "false");
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/JSON'
-            }
-        };
-        fetch('/api/user/logout', options)
-            .then(response => {
-                console.log(response)
-            });
-    }
     return (
         <body>
-            <nav className='nav'>
-                <a href="/"><img src={myImage} alt="" /></a>
-                <div className='navComp'>
-                    <ul>
-                        <li><a href="/notifications">Obavijesti</a></li>
-                        <li><a href="/moji_prijatelji">Moji Prijatelji</a></li>
-                        <li><a href="/attended">Pohađani Eventi</a></li>
-                        {(userData.moderator === true) ? (<li><a href="/userActions">Upravljaj korisnicima</a></li>) : ('')}
-                        {(userData.admin === true) ? (<li><a href="/admin">Administrativne ovlasti</a></li>) : ('')}
-                        <div className='userInfo'>
-                            {(userData.username !== '') ? (<li><a href="/profile">{userData.username}</a></li>) : ('')}
-                            <li><a href="/" onClick={odjavi}>Odjava</a></li>
-                        </div>
-                    </ul>
-                </div>
-            </nav>
+            <Navbar />
             <div id="container">
                 <a>Nemate novih obavijesti</a>
                 <a href="/">Povratak</a>
             </div>
         </body>
-      );
+    );
 }
 
 export default Notifications;
